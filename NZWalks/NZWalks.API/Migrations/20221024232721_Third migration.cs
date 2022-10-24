f25c6c7a96391ff24a4ed2c50beb2573b8f09557
henryestrada <henryestrada92@gmail.com>
2022-10-24 17:30:21 -0600

    End of section 2

---
+++ NZWalks/NZWalks.API/Migrations/20221024232721_Third migration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    public partial class Thirdmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lenght",
+                table: "Walks",
+                newName: "Length");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Walks",
+                newName: "lenght");
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Models.Domain;
+
+public class WalkDifficulty
+{
+    public WalkDifficulty()
+    {
+        Code = string.Empty;
+    }
+
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+}
